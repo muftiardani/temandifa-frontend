d928873d1028e23f341bf8a2931ea5d8cd245d41
Muhammad Mufti Ardani <mufti4rdani@gmail.com>
2025-09-28 19:43:02 +0700

    API versioning

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 export const Config = {
   api: {
     baseUrl: process.env.EXPO_PUBLIC_API_BASE_URL,
-    detectUrl: `${process.env.EXPO_PUBLIC_API_BASE_URL}/api/detect`,
-    scanUrl: `${process.env.EXPO_PUBLIC_API_BASE_URL}/api/scan`,
-    transcribeUrl: `${process.env.EXPO_PUBLIC_API_BASE_URL}/api/transcribe`,
+    detectUrl: `${process.env.EXPO_PUBLIC_API_BASE_URL}/api/v1/detect`,
+    scanUrl: `${process.env.EXPO_PUBLIC_API_BASE_URL}/api/v1/scan`,
+    transcribeUrl: `${process.env.EXPO_PUBLIC_API_BASE_URL}/api/v1/transcribe`,
   },
   agora: {
     appId: process.env.EXPO_PUBLIC_AGORA_APP_ID,
